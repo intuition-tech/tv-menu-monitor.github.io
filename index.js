33cd67e4e0a49297875368642d7d930b6633a382
Maria Veres <veres.maria.dev@gmail.com>
2024-12-19 11:29:01 +0300

    add Copy Title function

--- index.js
+++ index.js
@@ -1,25 +1,17 @@
-// копировать название макета по клику 
+// скопировать название макета по клику 
+const titlesArray = Array.from(document.getElementsByClassName('title'));
 
-let title = document.getElementsByClassName('quote-text');
+function copyTitle(title) {
+  title.addEventListener('click', () => {
+    const text = title.textContent;
+    navigator.clipboard.writeText(text);
+    title.innerText = 'Скопировано';
+    setTimeout(() => {
+      
+      title.innerText = text; // надпись 'Скопировано' исчезает через 1 сек
+    }, 1000);
+    
+  });
+}
 
-
-title.addEventListener('click', () => {
-	let text = title.textContent;
-  navigator.clipboard.writeText(text);
-	alert(`Скопировано в буфер обмена`);
-  title.innerText = 'Скопировано в буфер обмена';
-});
-
-// Дан массив с числами. Найдите сумму квадратных корней элементов этого массива.
-// let arr = [1, 2, 8];
-
-// function toSqrt(el) {
-//   return (Math.sqrt(el))
-// }
-// arr.forEach(toSqrt);
-
-// let arr2 = arr.map(toSqrt);
-// // console.log(arr2);
-
-// let sumOfSqrts = arr2.reduce((acc, el) => acc + el, 0);
-// console.log(sumOfSqrts);
+titlesArray.forEach(copyTitle);
