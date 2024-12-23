90c26e734b0d7071e7ebabfffdbfa5699046344c
Maria Veres <veres.maria.dev@gmail.com>
2024-12-23 15:04:36 +0300

    feat: add toggle for vertical input

--- index.js
+++ index.js
@@ -15,3 +15,45 @@ function copyTitle(title) {
 }
 
 titlesArray.forEach(copyTitle);
+
+// видимость контента при фильтрации 
+const toggleVisibility = (showElement, hideElement) => {
+  showElement.classList.remove('hide'); // Показать элемент
+  hideElement.classList.add('hide');    // Скрыть элемент
+};
+
+// Переключение формата (ветрикальные / горизонтальные)
+const verticalButton = document.getElementById('vertical');
+const horizontalButton = document.getElementById('horizontal');
+
+verticalButton.addEventListener('change', () => {
+  toggleVisibility(
+    document.querySelector('.vertical'),
+    document.querySelector('.horizontal')
+  );
+});
+horizontalButton.addEventListener('change', () => {
+  toggleVisibility(
+    document.querySelector('.horizontal'),
+    document.querySelector('.vertical')
+  );
+});
+
+
+// Переключение на период (предыдущий / настоящий) 
+// const previous = document.getElementById('previous');
+// const present = document.getElementById('present');
+
+// previous.addEventListener('change', () => {
+//   toggleVisibility(
+//     document.querySelector('.previous .vertical'),
+//     document.querySelector('.present .vertical'),
+//     console.log('prev')
+//   );
+// });
+// present.addEventListener('change', () => {
+//   toggleVisibility(
+//     document.querySelector('.present .vertical'),
+//     document.querySelector('.previous .vertical')
+//   );
+// });
